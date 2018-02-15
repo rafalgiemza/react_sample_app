999e9c4d900848b2f7647df03a7785730ee67cbf
giemza <rafalgiemza@gmail.com>
2018-02-15 15:52:50 +0100

    Initial commit after basic tutorial done

---
+++ src/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+
+export default () => (
+    <Card>
+        <CardHeader title="Welcome to the administration" />
+        <CardText>Lorem ipsum sic dolor amet...</CardText>
+    </Card>
+);
